45678271d050e7ac5d5aaf60a928ece9cb9a6e6d
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2025-02-03 16:54:35 +0000

    First commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// função para aparecer os desejos
+
+const photoBall = document.querySelector('.dragon-ball');
+const wishesList = document.querySelector('.wishes-Gui');
+
+if (photoBall){
+    photoBall.onclick =()=>{
+        wishesList.classList.remove('d-none');
+    };
+}
+
+// fim da função para aparecer os desejos
+
+// função para aparecer os modals após o desejo selecionado
+
+const modalAboutGui= document.querySelector('.modal1');
+const wisheAboutGui= document.querySelector('.wishe1');
+const modalExperience= document.querySelector('.modal2');
+const wisheExperience= document.querySelector('.wishe2');
+const modalLenguage= document.querySelector('.modal3');
+const wisheLenguage= document.querySelector('.wishe3');
+const closeModal= document.querySelector('.closeModal');
+const modals = document.querySelector('.modals');
+
+if(wisheAboutGui){
+    wisheAboutGui.onclick=()=>{
+        modalAboutGui.classList.remove('d-none');
+        closeModal.classList.remove('d-none');
+        modals.classList.remove('d-none');
+    }
+};
+
+if(wisheExperience){
+    wisheExperience.onclick=()=>{
+        modalExperience.classList.remove('d-none');
+        closeModal.classList.remove('d-none');
+        modals.classList.remove('d-none');
+    }
+};
+
+if(wisheLenguage){
+    wisheLenguage.onclick=()=>{
+        modalLenguage.classList.remove('d-none');
+        closeModal.classList.remove('d-none');
+        modals.classList.remove('d-none');
+    }
+};
+
+if(closeModal){
+    closeModal.onclick=()=>{
+        closeModal.classList.add('d-none');
+        modalAboutGui.classList.add('d-none');
+        modalExperience.classList.add('d-none');
+        modalLenguage.classList.add('d-none');
+        modals.classList.add('d-none');
+    }
+}
+
+// fim da função dos modals
+
+// Função das esferas
+
+// Fim da função das esferas
