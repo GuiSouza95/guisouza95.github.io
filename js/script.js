b2a0b2642334ed08ffeda38faa748e72b0170d75
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2025-02-05 00:52:08 +0000

    audio, mensagens, shenlong e modais a funcionar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // função para aparecer os desejos
 
-const photoBall = document.querySelector('.dragon-ball');
+const Shenlong = document.querySelector('.Shenlong');
 const wishesList = document.querySelector('.wishes-Gui');
 
-if (photoBall){
-    photoBall.onclick =()=>{
+if (Shenlong){
+    Shenlong.onclick =()=>{
         wishesList.classList.remove('d-none');
+        wishesList.classList.add('bg-danger');
     };
 }
 
@@ -19,8 +20,11 @@ const modalExperience= document.querySelector('.modal2');
 const wisheExperience= document.querySelector('.wishe2');
 const modalLenguage= document.querySelector('.modal3');
 const wisheLenguage= document.querySelector('.wishe3');
+const downCV = document.querySelector('.modal4');
+const wisheCV = document.querySelector('.wishe4');
 const closeModal= document.querySelector('.closeModal');
 const modals = document.querySelector('.modals');
+
 
 if(wisheAboutGui){
     wisheAboutGui.onclick=()=>{
@@ -41,6 +45,14 @@ if(wisheExperience){
 if(wisheLenguage){
     wisheLenguage.onclick=()=>{
         modalLenguage.classList.remove('d-none');
+        closeModal.classList.remove('d-none');
+        modals.classList.remove('d-none');
+    }
+};
+
+if(wisheCV){
+    wisheCV.onclick=()=>{
+        downCV.classList.remove('d-none');
         closeModal.classList.remove('d-none');
         modals.classList.remove('d-none');
     }
@@ -105,6 +117,7 @@ if(sphereDragon){
                 setTimeout(function(){
                     document.querySelector('.Shenlong').classList.remove('d-none');
                     document.querySelector('body').classList.add('bg-black');
+                    document.querySelector('.bg-cloud').classList.add('d-none');
                 }, 3000);
             }
         }
@@ -122,20 +135,29 @@ const frase4 = document.querySelector('.frase4');
 const frase5 = document.querySelector('.frase5');
 const frase6 = document.querySelector('.frase6');
 const frase7 = document.querySelector('.frase7');
-const frase8 = document.querySelector('.frase8');
+
+const playAudioGoku = document.querySelector('.Goku-ssj');
 
 const audio = document.getElementById('audio');
+
+let countFrase = 1;
 
 let audioPlaying = false;
 
 function playAudio(inicio,fim){
     if(audioPlaying){
         audio.pause();
+        audioPlaying = false;
     }
 
     audio.currentTime = inicio;
-    audio.play();
-    audioPlaying = true;
+    audio.play()
+        .then(()=>{
+            audioPlaying = true;
+        })
+        .catch((error) => {
+            console.error('Erro ao tentar reproduzir o áudio:', error);
+        });
 
     setTimeout(function(){
         audio.pause();
@@ -143,41 +165,79 @@ function playAudio(inicio,fim){
     }, (fim-inicio)*1000);
 }
 
-frase1.addEventListener('click', function(){
-    playAudio(0,2);
-})
-frase2.addEventListener('click', function(){
-    playAudio(3,6);
-})
-frase3.addEventListener('click', function(){
-    playAudio(7,11);
-})
-frase4.addEventListener('click', function(){
-    playAudio(12,15);
-})
-frase5.addEventListener('click', function(){
-    playAudio(16,18);
-})
-frase6.addEventListener('click', function(){
-    playAudio(19,21);
-})
-frase7.addEventListener('click', function(){
-    playAudio(22,27);
-})
-frase8.addEventListener('click', function(){
-    playAudio(28,35);
-});
+function nextPhrase(){
+    countFrase++;
+
+    if(countFrase === 2){
+        playAudio(2,7);
+        frase1.classList.add('d-none');
+        frase2.classList.remove('d-none');
+    }else if(countFrase === 3){
+        playAudio(7,12);
+        frase2.classList.add('d-none');
+        frase3.classList.remove('d-none');
+    }else if(countFrase === 4){
+        playAudio(12,19);
+        frase3.classList.add('d-none');
+        frase4.classList.remove('d-none');
+    }else if(countFrase === 5){
+        playAudio(19,22);
+        frase4.classList.add('d-none');
+        frase5.classList.remove('d-none');
+    }else if(countFrase === 6){
+        playAudio(22,28);
+        frase5.classList.add('d-none');
+        frase6.classList.remove('d-none');
+    }else if(countFrase === 7){
+        playAudio(28,35);
+        frase6.classList.add('d-none');
+        frase7.classList.remove('d-none');
+    }
+}
+
+    frase1.addEventListener('click', function(){
+        playAudio(0,2);
+        nextPhrase();
+    })
+    frase2.addEventListener('click', function(){
+        playAudio(2,7);
+        nextPhrase();
+    })
+    frase3.addEventListener('click', function(){
+        playAudio(7,12);
+        nextPhrase();
+    })
+    frase4.addEventListener('click', function(){
+        playAudio(12,19);
+        nextPhrase();
+    })
+    frase5.addEventListener('click', function(){
+        playAudio(19,22);
+        nextPhrase();
+    })
+    frase6.addEventListener('click', function(){
+        playAudio(22,28);
+        nextPhrase();
+    })
+    frase7.addEventListener('click', function(){
+        playAudio(28,35);
+        nextPhrase();
+    });
 
 window.onload = function(){
     setTimeout(function(){
         playAudio(0,2);
-    }, 3000);
+    }, 2000);
 }
 
 // Fim da função da fala do Goku
 
-// Função de trocar falas do Goku
+// Função fala do Goku
 
-
+if(playAudioGoku){
+    playAudioGoku.onclick=()=>{
+        playAudio(0,2);
+    }
+}
 
 // Fim da função de trocar falas do Goku
